0cafdbdbc810a120e41db270c923fa862ce8dfcb
Joaquin <65967417+joaquingavilan@users.noreply.github.com>
2024-10-22 18:33:18 -0300

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -42,6 +42,7 @@ def get_response(request):
 
 
 # Vista para recibir mensajes desde Twilio y devolver un mensaje de éxito
+@csrf_exempt
 def receive_whatsapp(request):
     if request.method == 'POST':
         # Obtener el mensaje entrante desde Twilio (por ahora no lo usaremos)
